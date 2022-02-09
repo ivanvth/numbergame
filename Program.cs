2237ecf55c6240227deb313db1b5c5dfb6b94e13
Ivan Thrysøe <ivanvt@gmail.com>
2022-02-09 23:51:44 +0100

    First commit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NumberGuessingGame {
+    class Program {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int winNum = r.Next(0, 100);
+            bool win = false;
+
+            do {
+                System.Console.Write("Guess a number between 0 and 100: ");
+                string s = System.Console.ReadLine();
+                int i = int.Parse(s);
+
+                if (i > winNum) {
+                    System.Console.WriteLine("Too high, try again!");
+                } else if (i < winNum) {
+                    System.Console.WriteLine("Too low, try again!");
+                } else {
+                    System.Console.WriteLine("You win!");
+                    win = true;
+                }
+            } while ( win == false);
+        }
+    }
+}
